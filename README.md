17abdb760575667cf1b944789854938cee81f87d
Sherief Elzoghby <szoghby@gmail.com>
2022-02-09 16:00:18 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Dentalprenr Video App
+
+A basic video conference app with Twilio Video. This app will generate a passcode-protected conference room and you can enter, leave, and invite others [Check out this code exchange for a walkthrough of the app](https://www.twilio.com/code-exchange/basic-video-chat?language=functions). The APP is customised with styling and branding of [Dentalprenr](https://dentalprenr.com/)
+
+## Pre-requisites
+
+### Environment variables
+
+This project requires some environment variables to be set. To keep your tokens and secrets secure, make sure to not commit the `.env` file in git. When setting up the project with `twilio serverless:init ...` the Twilio CLI will create a `.gitignore` file that excludes `.env` from the version history.
+
+In your `.env` file, set the following values:
+
+| Variable     | Description                                                                       | Required |
+| :----------- | :-------------------------------------------------------------------------------- | :------- |
+| `ACCOUNT_SID`| Find in the [console](https://www.twilio.com/console)                             | Yes      |
+| `API_KEY`    | Twilio API Key. Create one here (https://www.twilio.com/console/runtime/api-keys) | Yes      |
+| `API_SECRET` | Twilio API Secret corresponding to your API Key                                   | Yes      |
+| `PASSCODE`   | A passcode to gate your video call                                                | Yes      |
+
+### Function Parameters
+
+`/video-token` expects the following parameters:
+
+| Parameter | Description                                  | Required |
+| :-------- | :------------------------------------------- | :------- |
+| passcode  | The passcode the user entered on the website | Yes      |
+| roomName  | The roomName the user want to start conference on the website | Yes      |
+
+## Create a new project with the template
+
+1. Install the [Twilio CLI](https://www.twilio.com/docs/twilio-cli/quickstart#install-twilio-cli)
+2. Install the [serverless toolkit](https://www.twilio.com/docs/labs/serverless-toolkit/getting-started)
+
+```shell
+twilio plugins:install @twilio-labs/plugin-serverless
+```
+
+3. Initiate a new project
+
+```
+twilio serverless:init example --template=video && cd example
+```
+
+4. Start the server with the [Twilio CLI](https://www.twilio.com/docs/twilio-cli/quickstart):
+
+```
+twilio serverless:start
+```
+
+5. Open the web page at https://localhost:3000/index.html to test the app
+
+ℹ️ Check the developer console and terminal for any errors, make sure you've set your environment variables.
+
+## Deploying
+
+Deploy your functions and assets with either of the following commands. Note: you must run these commands from inside your project folder. [More details in the docs.](https://www.twilio.com/docs/labs/serverless-toolkit)
+
+With the [Twilio CLI](https://www.twilio.com/docs/twilio-cli/quickstart):
+
+```
+twilio serverless:deploy
+```
+
+## Screen Shots
+![Landing Page](readme/landing-page.png)
+![One Participant](readme/one-participant.png)
+![Three Participants](readme/three-participants.png)
---
+++ assets/conference.js
@@ -0,0 +1,228 @@
+// Keeps track of video elements and their event listeners
+const videoElements = {};
+
+function copyMeetingLink() {
+  /* Get the text field */
+  var copyText = document.getElementById("room-uuid");
+
+  /* Select the text field */
+  copyText.select();
+  //copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(`${window.location.origin}/conference.html?roomUUID=${copyText.value}`);
+
+  /* Alert the copied text */
+  //alert("Copied the text: " + copyText.value);
+}
+
+// Listen to onPlay and onPause events and intelligently re-attach the video element
+function shimVideoElement(track, el) {
+  let wasInterrupted = false;
+
+  const onPause = () => {
+    wasInterrupted = true;
+  };
+
+  const onPlay = () => {
+    if (wasInterrupted) {
+      track.detach(el);
+      track.attach(el);
+      wasInterrupted = false;
+    }
+  };
+
+  el.addEventListener('pause', onPause);
+  el.addEventListener('play', onPlay);
+
+  // Track this element so we can remove the listeners
+  videoElements[el] = { onPause, onPlay };
+}
+
+const getPasscode = () => {
+  const passcodeInput = document.getElementById('passcode') || {};
+  const passcode = passcodeInput.value;
+  passcodeInput.value = '';
+
+  return passcode;
+};
+
+const getParamValue = (paramName) => {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  const roomName = params[paramName];
+  return roomName;
+};
+
+const trackSubscribed = (div, track) => {
+  div.appendChild(track.attach());
+
+  track.attach(div);
+  shimVideoElement(track, div);
+};
+
+const trackUnsubscribed = (div, track) => {
+  const { onPause, onPlay } = videoElements[div];
+  div.removeEventListener('pause', onPause);
+  div.removeEventListener('play', onPlay);
+  track.detach(div);
+  /*
+  track.detach().forEach((element) => {
+    element.remove();
+  });
+  */
+};
+
+// connect participant
+const participantConnected = (participant) => {
+  console.log(`Participant '${participant.identity}' connected`);
+
+  const statusDiv = document.getElementById('status');
+  const newStatus = document.createElement('div'); // create div for new participant
+  newStatus.id = participant.sid + 'status';
+  newStatus.innerHTML = `Participant '${participant.identity}' connected`;
+  statusDiv.appendChild(newStatus);
+
+  const participantsDiv = document.getElementById('participants');
+  const div = document.createElement('div'); // create div for new participant
+  div.id = participant.sid;
+
+  participant.on('trackSubscribed', (track) => {
+    trackSubscribed(div, track);
+  });
+
+  participant.on('trackUnsubscribed', (track) => trackUnsubscribed(div, track));
+
+  participant.tracks.forEach((publication) => {
+    if (publication.isSubscribed) {
+      trackSubscribed(div, publication.track);
+    }
+  });
+  participantsDiv.appendChild(div);
+};
+const participantDisconnected = (participant) => {
+  console.log(`Participant '${participant.identity}' disconnected.`);
+  const statusDiv = document.getElementById('status');
+  const newStatus = document.createElement('div'); // create div for new participant
+  newStatus.id = participant.sid + 'status';
+  newStatus.innerHTML = `Participant '${participant.identity}' disconnected.`;
+  statusDiv.appendChild(newStatus);
+
+  document.getElementById(participant.sid).remove();
+};
+
+(() => {
+  const { Video } = Twilio;
+  let videoRoom;
+  let localStream;
+  const video = document.getElementById('video');
+
+  // preview screen
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: true })
+    .then((vid) => {
+      video.srcObject = vid;
+      localStream = vid;
+    });
+
+  const joinRoomButton = document.getElementById('button-join');
+  const leaveRoomButton = document.getElementById('button-leave');
+  const roomControlsForm = document.getElementById('room-controls-form');
+  const preConnectControls = document.getElementById('pre-connect-controls');
+  const postConnectControls = document.getElementById('post-connect-controls');
+  const participantsDiv = document.getElementById('participants');
+  const permissionsHelp = document.getElementById('permissions-help');
+  const roomNameDiv = document.getElementById('room-name');
+  const roomUUIDDiv = document.getElementById('room-uuid');
+  const copyLink = document.getElementById('copy-link');
+
+  var roomNameUUID = '';
+  if (getParamValue('hostName')) {
+    roomNameUUID = `${getParamValue('hostName').replaceAll(' ', '.')}.${Math.random().toString(36).substring(2, 15)}${Math.random().toString(36).substring(2, 15)}`;
+    const userName = document.getElementById('username');
+    userName.value = getParamValue('hostName');
+    userName.disabled = true;
+  }
+  else {
+    roomNameUUID = getParamValue('roomUUID');
+  }
+  roomUUIDDiv.value = roomNameUUID;
+  roomNameDiv.innerHTML = `Welcome to '${roomNameUUID}' room`;
+
+  const joinRoom = (event) => {
+    const statusDiv = document.getElementById('status');
+    statusDiv.innerHTML = "";
+    event.preventDefault();
+
+    // get access token
+    const userName = document.getElementById('username').value;
+    fetch(`video-token?passcode=${getPasscode()}&userName=${userName}&roomName=${roomUUIDDiv.value}`)
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        console.error(resp);
+        if (resp.status === 401) {
+          throw new Error('Invalid passcode');
+        } else {
+          throw new Error('Unexpected error. Open dev tools for logs');
+        }
+      })
+      .then((body) => {
+        const { token } = body;
+        //console.log(token);
+        // connect to room
+        return Video.connect(token, { name: roomUUIDDiv.value });
+      })
+      .then((room) => {
+        console.log(`Connected to '${room.name}:${room.sid}' room`);
+        const statusDiv = document.getElementById('status');
+        const newStatus = document.createElement('div'); // create div for new participant
+        newStatus.innerHTML = `Connected to room '${room.name}''`;
+        statusDiv.appendChild(newStatus);
+
+        videoRoom = room;
+        room.participants.forEach(participantConnected);
+        room.on('participantConnected', participantConnected);
+
+        room.on('participantDisconnected', participantDisconnected);
+        room.once('disconnected', (error) =>
+          room.participants.forEach(participantDisconnected)
+        );
+        preConnectControls.style.display = 'none';
+        permissionsHelp.style.display = 'none';
+        postConnectControls.style.display = 'inline-block';
+        participantsDiv.style.display = 'flex';
+
+        if(getParamValue('hostName')) {
+          copyLink.style.display = 'block';
+        }
+        else {
+          copyLink.style.display = 'none';
+        }
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-alert
+        alert(err.message);
+      });
+  };
+
+  //roomControlsForm.onsubmit = joinRoom;
+  joinRoomButton.onclick = joinRoom;
+
+  // leave room
+  leaveRoomButton.onclick = (event) => {
+    videoRoom.disconnect();
+    console.log(`Disconnected from Room ${videoRoom.name}`);
+    const statusDiv = document.getElementById('status');
+    const newStatus = document.createElement('div'); // create div for new participant
+    newStatus.innerHTML = `Disconnected from Room ${videoRoom.name}`;
+    statusDiv.appendChild(newStatus);
+
+    preConnectControls.style.display = 'inline-block';
+    permissionsHelp.style.display = 'block';
+    postConnectControls.style.display = 'none';
+    participantsDiv.style.display = 'none';
+    event.preventDefault();
+  };
+})();
